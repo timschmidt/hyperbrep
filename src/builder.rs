98f9cb90da426aef6aaf2937780f1d588a0fa002
Timothy Schmidt <timschmidt@gmail.com>
2026-07-30 03:18:30 -0400

    measure fully split quartic weight revolutions

--- src/builder.rs
+++ src/builder.rs
@@ -6212,6 +6212,48 @@ mod tests {
     }
 
     #[test]
+    fn revolution_path_measures_fully_split_quartic_weight_profile() {
+        let cp = |x, y| CurvePoint2::new(r(x), r(y));
+        let rational = hypercurve::RationalBezier2::try_new(
+            vec![cp(4, 0), cp(6, 0), cp(6, 1), cp(6, 2), cp(4, 2)],
+            vec![Real::one(), r(2), r(4), r(8), r(16)],
+        )
+        .unwrap();
+        let profile = CurvePath2::try_new(vec![
+            Curve2::from(LineSeg2::try_new(cp(2, 0), cp(4, 0)).unwrap()),
+            Curve2::from(rational),
+            Curve2::from(LineSeg2::try_new(cp(4, 2), cp(2, 2)).unwrap()),
+            Curve2::from(LineSeg2::try_new(cp(2, 2), cp(2, 0)).unwrap()),
+        ])
+        .unwrap();
+        let expected = (r(59_128) * Real::pi() / r(1_155)).unwrap();
+        let (model, solid) = revolve_path(&profile).unwrap();
+        assert_eq!(
+            compare_reals(&model.solid_volume(solid).unwrap(), &expected).value(),
+            Some(std::cmp::Ordering::Equal)
+        );
+        assert_eq!(
+            model.classify_point(solid, &p(3, 0, 1)).unwrap(),
+            SolidPointLocation::Inside
+        );
+        assert_eq!(
+            model.classify_point(solid, &p(6, 0, 1)).unwrap(),
+            SolidPointLocation::Outside
+        );
+
+        let json = model.to_json().unwrap();
+        let rebuilt = crate::RawModel::from_json(&json)
+            .unwrap()
+            .validate()
+            .unwrap();
+        assert_eq!(rebuilt.to_json().unwrap(), json);
+        assert_eq!(
+            compare_reals(&rebuilt.solid_volume(solid).unwrap(), &expected).value(),
+            Some(std::cmp::Ordering::Equal)
+        );
+    }
+
+    #[test]
     fn revolution_path_rejects_exact_self_crossings_before_topology_build() {
         let cp = |x, y| CurvePoint2::new(r(x), r(y));
         let profile = CurvePath2::try_new(vec![
